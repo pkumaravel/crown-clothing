c2e1f1f08f182eb0f42a45227bd5cbf4a56fc75a
Kumaravel Panneerselvam <kp@ets.org>
2020-08-23 00:40:54 -0400

    adding shop page with collection-preview and colletion components

--- src/App.js
+++ src/App.js
@@ -3,23 +3,16 @@ import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatPage = () => {
-  return (
-    <div>
-      <h1>Hat Page</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Link to="/">Home</Link>
-      <Link to="/hats">Hats</Link>
+      <Link to="/shop">Shop</Link>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
